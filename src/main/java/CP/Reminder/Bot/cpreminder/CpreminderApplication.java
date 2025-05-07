7b7e51c0c0304ef21b8fcf8ae02e7750db51a837
Ulvi Sadigzade <ulvisadiqzade05@gmail.com>
2025-05-07 14:54:28 +0400

    Inital Commit

---
+++ src/main/java/CP/Reminder/Bot/cpreminder/CpreminderApplication.java
@@ -0,0 +1,13 @@
+package CP.Reminder.Bot.cpreminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpreminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpreminderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CP/Reminder/Bot/cpreminder/CpreminderApplicationTests.java
@@ -0,0 +1,13 @@
+package CP.Reminder.Bot.cpreminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpreminderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
